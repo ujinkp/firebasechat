d0e09babaff9ffe06f5b72ef7840978d2f44d871
Ujin <valikkp@gmail.com>
2020-07-15 13:38:11 +0300

    next

--- App.js
+++ App.js
@@ -1,61 +1,93 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { Image } from 'react-native';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
+import {Image} from 'react-native';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+// import {NavigationContainer} from '@react-navigation/native';
+// import {createStackNavigator} from '@react-navigation/stack';
 
-import { Routes } from './screens/Routes';
+import {Routes} from './screens/Routes';
 import LoginScreen from './screens/LoginScreen';
-import HomeScreen from './screens/HomeScreen';
-import AuthLoadingScreen from './screens/AuthLoadingScreen';
-import ChatScreen from './screens/ChatScreen';
-import ProfileScreen from './screens/ProfileScreen';
+// import HomeScreen from './screens/HomeScreen';
+// import AuthLoadingScreen from './screens/AuthLoadingScreen';
+// import ChatScreen from './screens/ChatScreen';
+// import ProfileScreen from './screens/ProfileScreen';
 
 const AppStack = createStackNavigator({
   // Home: HomeScreen,
   // Chat: ChatScreen,
-  [Routes.Home]: require("./screens/HomeScreen").default,
-  [Routes.Chat]: require("./screens/ChatScreen").default,
+  [Routes.Home]: require('./screens/HomeScreen').default,
+  [Routes.Chat]: require('./screens/ChatScreen').default,
 });
+
+// const AppStack = createStackNavigator();
 
 AppStack.navigationOptions = ({navigation}) => {
   let tabBarVisible = navigation.state.index === 0;
 
   return {
-    tabBarVisible
-  }
-}
+    tabBarVisible,
+  };
+};
 
-const AuthStack = createStackNavigator({ Login: LoginScreen });
+const AuthStack = createStackNavigator({Login: LoginScreen});
+// const AuthStack = createStackNavigator();
 
-const TabNavigator = createBottomTabNavigator({
-  // Chats: AppStack,
-  // Profile: ProfileScreen,
-  [Routes.Chats]: AppStack,
-  [Routes.Profile]: require("./screens/ProfileScreen").default,
-}, {
-  defaultNavigationOptions: ({ navigation }) => ({
-    tabBarIcon: ({ focused, horizontal, tintColor }) => {
-      const { routeName } = navigation.state;
-      let imageName = require('./images/chats.png');
-      if (routeName === 'Profile') {
-        imageName = require('./images/settings.png');
-      }
-
-      // You can return any component that you like here!
-      return <Image source={imageName} style={{width: 25, resizeMode: 'contain', tintColor}} />;
-    },
-  })
-});
-
-export default createAppContainer(createSwitchNavigator(
+const TabNavigator = createBottomTabNavigator(
   {
-    // AuthLoading: AuthLoadingScreen,
-    [Routes.AuthLoading]: require("./screens/AuthLoadingScreen").default,
-    App: TabNavigator,
-    Auth: AuthStack,
+    // Chats: AppStack,
+    // Profile: ProfileScreen,
+    [Routes.Chats]: AppStack,
+    [Routes.Profile]: require('./screens/ProfileScreen').default,
   },
   {
-    initialRouteName: 'AuthLoading',
-  }
-))
+    defaultNavigationOptions: ({navigation}) => ({
+      tabBarIcon: ({focused, horizontal, tintColor}) => {
+        const {routeName} = navigation.state;
+        let imageName = require('./images/chats.png');
+        if (routeName === 'Profile') {
+          imageName = require('./images/settings.png');
+        }
+
+        // You can return any component that you like here!
+        return (
+          <Image
+            source={imageName}
+            style={{width: 25, resizeMode: 'contain', tintColor}}
+          />
+        );
+      },
+    }),
+  },
+);
+
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      // AuthLoading: AuthLoadingScreen,
+      [Routes.AuthLoading]: require('./screens/AuthLoadingScreen').default,
+      App: TabNavigator,
+      Auth: AuthStack,
+    },
+    {
+      initialRouteName: 'AUTHLOADING',
+    },
+  ),
+);
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <AppStack.Navigator>
+//         <AppStack.Screen
+//           name={[Routes.AuthLoading]}
+//           component={[Routes.AuthLoading]}
+//         />
+//       </AppStack.Navigator>
+//       <AuthStack.Navigator>
+//         <AuthStack.Screen name="Login" component={[Routes.LoginScreen]} />
+//       </AuthStack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
--- screens/AuthLoadingScreen.js
+++ screens/AuthLoadingScreen.js
@@ -1,45 +1,44 @@
 import React from 'react';
-import { ActivityIndicator, AsyncStorage, StatusBar, View } from 'react-native';
+import {ActivityIndicator, AsyncStorage, StatusBar, View} from 'react-native';
 import firebase from 'firebase';
 
 import User from '../User';
 
-
 export default class AuthLoadingScreen extends React.Component {
-    constructor(props) {
-        super(props);
-        this._bootstrapAsync();
-    }
+  constructor(props) {
+    super(props);
+    this._bootstrapAsync();
+  }
 
-    UNSAFE_componentWillMount() {
-        var config = {    
-            // Your web app's Firebase configuration
-        
-                apiKey: "AIzaSyAAMldk-mFe20w4__7uNCpUYyMgJF9wTts",
-                authDomain: "fir-chat-28f5a.firebaseapp.com",
-                databaseURL: "https://fir-chat-28f5a.firebaseio.com",
-                projectId: "fir-chat-28f5a",
-                storageBucket: "fir-chat-28f5a.appspot.com",
-                messagingSenderId: "297914044803",
-                appId: "1:297914044803:web:140cb5fccc19e72a4826b6"
-            };
-            if (!firebase.apps.length) {
-                firebase.initializeApp({config});
-             }
-        // firebase.initializeApp(config)
-    }
+  //   UNSAFE_componentWillMount() {
+  componentDidMount() {
+    var config = {
+      // Your web app's Firebase configuration
 
-    _bootstrapAsync = async () => {
-        User.phone = await AsyncStorage.getItem('userPhone');
-        this.props.navigation.navigate(User.phone ? 'App' : 'Auth');
+      apiKey: 'AIzaSyAAMldk-mFe20w4__7uNCpUYyMgJF9wTts',
+      authDomain: 'fir-chat-28f5a.firebaseapp.com',
+      databaseURL: 'https://fir-chat-28f5a.firebaseio.com',
+      projectId: 'fir-chat-28f5a',
+      storageBucket: 'fir-chat-28f5a.appspot.com',
+      messagingSenderId: '297914044803',
+      appId: '1:297914044803:web:140cb5fccc19e72a4826b6',
     };
-
-    render() {
-        return (
-            <View>
-                <ActivityIndicator />
-                <StatusBar barStyle="default" />
-            </View>
-        )
+    if (!firebase.apps.length) {
+      firebase.initializeApp({config});
     }
+  }
+
+  _bootstrapAsync = async () => {
+    User.phone = await AsyncStorage.getItem('userPhone');
+    this.props.navigation.navigate(User.phone ? 'App' : 'Auth');
+  };
+
+  render() {
+    return (
+      <View>
+        <ActivityIndicator />
+        <StatusBar barStyle="default" />
+      </View>
+    );
+  }
 }
